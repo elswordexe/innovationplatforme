6542211291fddcaa15f754755578ca7de951e61b
elswordexe <voltronakram@gmail.com>
2025-11-27 23:30:01 +0100

    fix

---
+++ microservices/run.bat
@@ -0,0 +1,26 @@
+@echo off
+title Microservices Launcher
+
+echo ============================================
+echo   STARTING SPRING MICRO-SERVICES SUITE
+echo ============================================
+
+REM
+echo Starting Eureka server...
+start cmd /k "cd eureka && mvnw spring-boot:run"
+
+REM --- Open API Gateway ---
+echo Starting API Gateway...
+start cmd /k "cd gateway && mvnw spring-boot:run"
+
+REM --- Open User Service ---
+echo Starting User Service...
+start cmd /k "cd userservice && mvnw spring-boot:run"
+
+echo Starting idea Service...
+start cmd /k "cd ideaservice && mvnw spring-boot:run"
+
+echo ============================================
+echo   All services started in separate windows.
+echo ============================================
+pause
