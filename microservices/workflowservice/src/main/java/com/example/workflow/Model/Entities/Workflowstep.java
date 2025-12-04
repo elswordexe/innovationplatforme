2569de43a7a687cc4409473e4f895d157865998d
elswordexe <voltronakram@gmail.com>
2025-12-04 13:52:20 +0100

    workflowsercice

---
+++ microservices/workflowservice/src/main/java/com/example/workflow/Model/Entities/Workflowstep.java
@@ -0,0 +1,27 @@
+package com.example.workflow.Model.Entities;
+
+import com.example.workflow.Model.Enums.StepType;
+import com.example.workflow.Model.Enums.WorkflowStatus;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkflowStep {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Idea idea;
+    private User approveur;
+    private StepType stepType;
+    private WorkflowStatus status;
+    private Date actiondate;
+    private String comments;
+}
---
+++ microservices/workflowservice/src/main/java/com/example/workflow/Model/Enums/WorkflowStatus.java
@@ -0,0 +1,9 @@
+package com.example.workflow.Model.Enums;
+
+public enum WorkflowStatus {
+    PENDING,
+    IN_PROGRESS,
+    APPROVED,
+    REJECTED,
+    CANCELLED
+    }
