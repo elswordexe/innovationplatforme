3990905edbeaf75563bd0a19c26bd6e9121d71a6
elswordexe <voltronakram@gmail.com>
2026-01-27 21:41:38 +0100

    sidebar

---
+++ angular/src/app/features/dash/dash.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from '../../core/components/sidebar/sidebar';
+
+@Component({
+  selector: 'app-dash',
+  imports: [Sidebar],
+  templateUrl: './dash.html',
+  styleUrl: './dash.css',
+})
+export class Dash {
+
+}
